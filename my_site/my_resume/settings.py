15a8f5c343135ac5ed5198cc2649706ec2e43ff0
control <control@mylinux.wf.lan>
2023-05-28 02:12:15 +0300

    Промежуточный вариант подготовки создания контейнера. Востановил работу

--- my_site/my_resume/settings.py
+++ my_site/my_resume/settings.py
@@ -25,15 +25,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY ='django-insecure-knp!rswtk&d^c_792a^)3e)fihtu@te4r97v98o!o#ot=k#oe)'
-SECRET_KEY = os.getenv("SECRET_KEY")
+#SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 #DEBUG = False
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+
+# 'DJANGO_ALLOWED_HOSTS' должен быть в виде одной строки с хостами разделенными символом пробела
+# Для примера: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+#ALLOWED_HOSTS = ['*']
 
 
 # Application definition
