2eda0dc50784231a425d7d7f2666c4856391bf45
control <control@mylinux.wf.lan>
2023-05-29 18:40:51 +0300

    Выполнил dev развёрывание по прмеру https://testdriven.io/blog/dockerizing-django-with-postgres-gunicorn-and-nginx/

--- my_site/my_resume/settings.py
+++ my_site/my_resume/settings.py
@@ -90,12 +90,32 @@ WSGI_APPLICATION = 'my_resume.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
+
 }
+
+
+
+
+
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
