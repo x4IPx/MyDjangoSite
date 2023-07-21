332bb5ce8324d09adc55e39e367ba185f6b0a31d
root <root@pop-os.localdomain>
2023-07-22 00:13:40 +0300

    feedback

--- my_site/forms.py
+++ my_site/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class TelegramForm(forms.Form):
-    TelegremMessage = forms.CharField(label="Обратная связь:", widget=forms.Textarea)
+    TelegremMessage = forms.CharField( label="" ,  help_text="Пожалуйста, расскажите, что именно вам не понравилось/смутило в моем резюме, а также поделитесь своими пожеланиями и рекомендациями. Я буду благодарен за ваш отзыв.", widget=forms.Textarea)
